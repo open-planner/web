5b6e9a80c54621022491730ddd4d14e269a935ad
joerverson santos <joerverson.santos@gmail.com>
2020-05-12 21:30:17 -0300

    feito a lista base

--- src/Views/Home/index.js
+++ src/Views/Home/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import ListDataLoading from '../../Components/List'
 
 export default class index extends Component {
   render() {
     return (
       <div>
-        home
+        <ListDataLoading />
       </div>
     )
   }
